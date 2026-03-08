e6f6436468d1c8878b2bf359737e48c5af395adc
StevenJmnz <stevenjr5826@gmail.com>
2026-03-08 10:29:48 -0400

    Agregar package.json y gitignore

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Web dashboard disponible en http://0.0.0.0:${port}`);
+});
 
 // Variables compartidas con el bot
 global.botState = {
